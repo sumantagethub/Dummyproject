aa50198e4a12de22d7e73fe21bf8ec07215944c3
sumantagethub <janasumanta721152@gmail.com>
2022-01-09 22:12:01 +0530

    java docsm

---
+++ src/test/java/test/_1_ClassConstructor.java
@@ -0,0 +1,23 @@
+package test;
+
+public class _1_ClassConstructor {
+	
+	int id;
+	String name;
+	
+	
+	public _1_ClassConstructor(int i,String n)  {
+		
+		System.out.println(i+"  "+n);
+		
+	}
+
+	public static void main(String[] args) {
+		_1_ClassConstructor s1 = new  _1_ClassConstructor(29, "desj");
+		_1_ClassConstructor s2 = new  _1_ClassConstructor(22, "desuej");
+		
+
+				
+		
+	}
+}
